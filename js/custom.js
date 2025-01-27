64f2955099f8d91e19a3b188463832cc3a21c2ac
erichari <exk.0yb@gmail.com>
2025-01-27 11:16:35 +0900

    edit html, js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,7 @@
 $(function(){
-    
+    $('img').click(function() {
+        var className = $(this).attr('class').split(" ")[1];
+        $("#"+className).get(0).play();
+        console.log(className);
+    });
 })
