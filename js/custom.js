c1e568bfad5eb1cbfbe91cd0d45b977da068aa0e
erichari <exk.0yb@gmail.com>
2025-01-28 21:59:33 +0900

    add animation to custom.js

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,6 @@ $(function(){
     $('img').click(function() {
         var className = $(this).attr('class').split(" ")[1];
         $("#"+className).get(0).play();
-        console.log(className);
+        $(this).animate({'width': '50%'}, 200).animate({'width': '60%'}, 200);
     });
 })
