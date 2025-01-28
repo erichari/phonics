52ce954cf7abe4223681dc586b59370e3c3aed0c
erichari <exk.0yb@gmail.com>
2025-01-28 23:01:28 +0900

    change image size

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,6 @@ $(function(){
     $('img').click(function() {
         var className = $(this).attr('class').split(" ")[1];
         $("#"+className).get(0).play();
-        $(this).animate({'width': '50%'}, 200).animate({'width': '60%'}, 200);
+        $(this).animate({'width': '40%'}, 200).animate({'width': '50%'}, 200);
     });
 })
