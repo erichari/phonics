235aa818e8d57433f27fc28bec4f5b83adc6966c
erichari <exk.0yb@gmail.com>
2025-01-31 08:33:41 +0900

    debug

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,6 @@
 $(function(){
-    $('img').on("click", function() {
+    var clickEventType=((window.ontouchstart!==null)?'click':'touchstart');
+    $('img').on(clickEventType, function() {
         var className = $(this).attr('class');
         $("#"+className).get(0).play();
         $(this).animate({'width': '40%'}, 200).animate({'width': '50%'}, 200);
