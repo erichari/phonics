9bb5e39174d563398431ca0d1082129e8d5db375
erichari <exk.0yb@gmail.com>
2025-01-30 21:31:24 +0900

    delete .capital

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(function(){
     $('img').click(function() {
-        var className = $(this).attr('class').split(" ")[1];
+        var className = $(this).attr('class');
         $("#"+className).get(0).play();
         $(this).animate({'width': '40%'}, 200).animate({'width': '50%'}, 200);
     });
