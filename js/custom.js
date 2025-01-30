253836306cbcc57be35d8dfa6b18d4eb1748a1ea
erichari <exk.0yb@gmail.com>
2025-01-31 08:39:39 +0900

    debug

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
 $(function(){
-    var clickEventType=((window.ontouchstart!==null)?'click':'touchstart');
+    var clickEventType = window.ontouchstart === null ? "touchstart" : "click";
     $('img').on(clickEventType, function() {
         var className = $(this).attr('class');
         $("#"+className).get(0).play();
